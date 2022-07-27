b39b687528361bd98f6b8276e5f50421d118c18b
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-07-27 13:03:34 +0300

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import './App.css';
-
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetails from './containers/ProductDetails';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h1>REDUX</h1>
-      <p>lets do this</p>
+      <Router>
+      <Header />
+      <Switch>
+      <Route path='/' exact component={ProductListing} />
+      <Route path='/product/:productId' exact component={ProductDetails} />
+      <Route>404 file no found</Route>
+      </Switch>
+      
+      </Router>
+      
+      
     </div>
   );
 }
---
+++ src/containers/ProductListing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductListing() {
+  return (
+    <div>
+        <h1>product listing</h1>
+    </div>
+  )
+}
+
+export default ProductListing
